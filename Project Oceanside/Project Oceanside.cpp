37cb2d427c74378bcadcae3b7ecec364d9737e56
fullgrowngaming <fullgrowngaming@gmail.com>
2019-12-29 18:15:29 -0800

    Uploading actor json files

--- Project Oceanside/Project Oceanside.cpp
+++ Project Oceanside/Project Oceanside.cpp
@@ -1,5 +1,6 @@
 #include "./Heap.h"
 #include "./Scene.h"
+#include <chrono>
 
 /*
 TODOs:
@@ -7,6 +8,9 @@ TODOs:
 2. implement checking system to compare addresses to a desired offset (i.e. 0x160 offset pot and chest)
 3. implement permutation generation and running those permutations
     -random? something else?
+4. implement ISoT and scarecrow memory leaks
+5. implement spawner behavior
+6. implement enemies or whatever dying and dropping things (tokens, magic drops, etc.)
 */
 
 const int START = 0x40b140;
@@ -18,8 +22,19 @@ int main()
     Heap* heap = new Heap(scene, START, END);
 
     heap->LoadRoom(0);
-    heap->ChangeRoom(1);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < 100000; i++)
+    {
+        heap->ChangeRoom(i % 2);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 	heap->PrintHeap();
+
+    std::cout << "Operation of 100000 loads ran in: " << duration.count() << " milliseconds" << std::endl;
+    std::cout << "Average room load:  " << duration.count()/ 100000.0f << " milliseconds" << std::endl;
 
     delete(scene);
     delete(heap);
